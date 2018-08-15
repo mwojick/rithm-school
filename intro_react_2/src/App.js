bb7d2dbcb8272f2e1d35d29751532210932e7a99
mwojick <35327565+mwojick@users.noreply.github.com>
2018-08-14 23:43:30 -0700

    add django demo

--- intro_react_2/src/App.js
+++ intro_react_2/src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import logo from './logo.svg';
-import './App.css';
-import Todo from './todo'
-import NewTodoForm from './newtodo'
+import "./App.css";
+import Todo from "./todo";
+import NewTodoForm from "./newtodo";
 
 class App extends Component {
   state = {
@@ -15,8 +15,8 @@ class App extends Component {
         title: "second",
         description: "second todo"
       }
-  ]
-  }
+    ]
+  };
 
   // handleNewTodo = newTodo => {
   //  this.setState({ todos: [newTodo, ...this.state.todos]} )
@@ -25,23 +25,26 @@ class App extends Component {
   // }
 
   // using the function form of updater
-  
-  handleNewTodo = newTodo => {
-  	this.setState( (prevState) => {
-  		return { todos: [newTodo, ...prevState.todos]}
-  	})
 
-  	// this.setState( (prevState) => {
-  	// 	return { todos: [newTodo, ...prevState.todos]}
-  	// })
-  }
+  handleNewTodo = newTodo => {
+    this.setState(prevState => {
+      // console.log(prevState);
+      return { todos: [newTodo, ...prevState.todos] };
+    });
+
+    // this.setState( (prevState) => {
+    //   console.log(prevState);
+    // 	return { todos: [newTodo, ...prevState.todos]}
+    // })
+  };
 
   render() {
-    let todos = this.state.todos.map( (todo, i) => <Todo key={i} todo={todo}/>)
+    // console.log("rendered")
+    let todos = this.state.todos.map((todo, i) => <Todo key={i} todo={todo} />);
 
     return (
       <div className="App">
-        <NewTodoForm addTodo={this.handleNewTodo}/>
+        <NewTodoForm addTodo={this.handleNewTodo} />
         {todos}
       </div>
     );
