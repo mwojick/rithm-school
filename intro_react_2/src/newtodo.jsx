c994175cf5e38c2e04c0a965bc6f1729e7b09719
mwojick <35327565+mwojick@users.noreply.github.com>
2018-09-15 22:55:34 -0700

    update

--- intro_react_2/src/newtodo.jsx
+++ intro_react_2/src/newtodo.jsx
@@ -1,27 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class NewTodoForm extends Component {
-
   handleSubmit = e => {
-    e.preventDefault()
+    e.preventDefault();
     let newTodo = {
       title: e.target.title.value,
       description: e.target.description.value
-    }
-    this.props.addTodo(newTodo)
-    
-    e.target.title.value = ""
-    e.target.description.value = ""
-  }
+    };
+    this.props.addTodo(newTodo);
+
+    e.target.title.value = "";
+    e.target.description.value = "";
+  };
 
   render() {
     return (
       <form action="" onSubmit={this.handleSubmit}>
-        <input type="text" name="title" placeholder="title"/>
-        <input type="text" name="description" placeholder="description"/>
-        <input type="submit" value="submit"/>
+        <input type="text" name="title" placeholder="title" />
+        <input type="text" name="description" placeholder="description" />
+        <input type="submit" value="submit" />
       </form>
-    )
+    );
   }
 }
 
